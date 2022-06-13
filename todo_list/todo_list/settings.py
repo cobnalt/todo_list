6fa1b8e5203b8ee724f62540d2cc64147b9a9233
Vladimir <cobalt1203@gmail.com>
2022-06-13 14:13:42 +0400

    homework_12

--- todo_list/todo_list/settings.py
+++ todo_list/todo_list/settings.py
@@ -85,9 +85,14 @@ WSGI_APPLICATION = 'todo_list.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'todo_list',
+        'USER': 'dante',
+        'PASSWORD': 'dante123456',
+        'HOST': 'db',
+        'PORT': '5432',
     }
+
 }
 
 
